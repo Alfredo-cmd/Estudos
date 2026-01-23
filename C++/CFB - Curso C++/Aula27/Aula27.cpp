1d77f578ebfb962dabd0f85a9cb873dfae1f72ba
Alfredo-cmd <alfredocontato4@gmail.com>
2026-01-23 17:59:28 -0300

    Curso Qt e Aulas

--- C++/CFB - Curso C++/Aula27/Aula27.cpp
+++ C++/CFB - Curso C++/Aula27/Aula27.cpp
@@ -3,7 +3,14 @@ using namespace std;
 
 int main(){
 
-    
+    //enum só inteiros, valor sequencial, por ex:
+    //com fuzil valendo 100, revolver vai valer 101 etc
+    enum armas{fuzil=100, revolver, rifle, escopeta};
+    armas armaSel;
+
+    armaSel=fuzil;
+
+    cout << armaSel << "\n";
 
     return 0;
 }
